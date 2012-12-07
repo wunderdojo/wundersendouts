0d921b67980cea81a1bd5fd28e67b0243feafbf8
wunderdojo <webmaster@wunderdojo.com>
2012-12-06 18:28:42 -0800

    Added README

--- wundersendouts.php
+++ wundersendouts.php
@@ -31,7 +31,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  * @version 1.0
 
  */
-
  include('wundersendoutswidget.php');
  add_action('widgets_init', array('wundersendoutsWidget','register_widget'));
 
